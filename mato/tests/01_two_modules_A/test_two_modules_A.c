c23473d0b224d615bca364a56d27d6c69f470ba5
pavelpetrovic <pavel.petrovic@gmail.com>
2020-08-31 20:55:44 +0200

    extracting printouts to separate function

--- mato/tests/01_two_modules_A/test_two_modules_A.c
+++ mato/tests/01_two_modules_A/test_two_modules_A.c
@@ -7,22 +7,8 @@
 
 int a1, a2;
 
-int main(int argc, char **argv)
+void print_list_of_modules()
 {
-    int this_node_id = 0;
-    if (argc > 1) sscanf(argv[1], "%d", &this_node_id);
-    
-    printf("initializing framework...\n");
-    mato_init(this_node_id);
-
-    printf("initializing module types...\n");
-    A_init();
-
-    printf("creating instances of modules...\n");
-
-    a1 = mato_create_new_module_instance("A", "A1");
-    a2 = mato_create_new_module_instance("A", "A2");
-	   
     printf("List of all modules:\n");
     GArray *modules_list = mato_get_list_of_all_modules();
     for(int i = 0; i < modules_list->len; i++)
@@ -40,7 +26,26 @@ int main(int argc, char **argv)
 		printf("%d: module_id=%d, type=%s, name=%s\n", i, info->module_id, info->type, info->name);
     }
     mato_free_list_of_modules(modules_list);
-	
+}
+
+int main(int argc, char **argv)
+{
+    int this_node_id = 0;
+    if (argc > 1) sscanf(argv[1], "%d", &this_node_id);
+    
+    printf("initializing framework...\n");
+    mato_init(this_node_id);
+
+    printf("initializing module types...\n");
+    A_init();
+
+    printf("creating instances of modules...\n");
+
+    a1 = mato_create_new_module_instance("A", "A1");
+    a2 = mato_create_new_module_instance("A", "A2");
+	   
+    print_list_of_modules();
+
     printf("starting...\n");
     mato_start();
 
